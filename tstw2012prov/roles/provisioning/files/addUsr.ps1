abe11cc643b3d3bf667199777f5ac0f7ddcb9ff6
Yomi Ogunyinka <Yomi.O@JaghoConsultants.co.uk>
2017-02-27 12:18:02 +0000

    added powershell script to add a user to the instance

---
+++ tstw2012prov/roles/provisioning/files/addUsr.ps1
@@ -0,0 +1,19 @@
+#ps1_sysnative
+
+if ($existing -eq $null) {
+
+    #   This will create a new local user $Username.
+    &   net user $Username $Password /add /Y /expires:never /passwordchg:NO /Y `
+    /comment:$grpDescription /fullname:$Fullname 
+
+    #   This will Add the local user to $Username to $group.
+    &   net localgroup $group $Username /add
+}
+else {
+    #   This will setting the password to the variable defined if the $Username already exists
+    $existing.SetPassword($Password)
+}
+    #   This will ensure that the password for $Username never expires
+    & WMIC USERACCOUNT WHERE "Name= '$Username'" SET PasswordExpires=FALSE
+    #   Here we will remove the user we created from the Users group
+    &   net localgroup Users $Username /delete
